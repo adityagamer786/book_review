6dd051345a69a04e667efdd5ac2c7ab902725f00
adityagamer786 <adityagamers786@gmail.com>
2019-10-14 14:15:44 +0530

    Added import.py

---
+++ import.py
@@ -0,0 +1,52 @@
+import os, csv
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+engine = create_engine(os.getenv("DATABSE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+engine.execute(
+ """CREATE TABLE books(
+      id SERIAL PRIMARY KEY,
+      isbn VARCHAR UNIQUE NOT NULL,
+      title VARCHAR NOT NULL,
+      author VARCHAR NOT NULL,
+      year INTEGER NOT NULL
+  )
+ """
+)
+
+engine.execute(
+  """CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    username VARCHAR NOT NULL,
+    name VARCHAR NOT NULL,
+    password VARCHAR NOT NULL
+   )
+  """
+)
+
+engine.execute(
+  """CREATE TABLE reviews(
+    id SERIAL PRIMARY KEY,
+    book_id INTEGER REFERENCES books,
+    user_id INTEGER REFERENCES users,
+    review VARCHAR NOT NULL,
+    rating INTEGER NOT NULL CHECK(rating >= 1 AND rating <= 5)
+   )
+  """
+)
+
+f = open('books.csv')
+books = csv.reader(f)
+
+for isbn, title, author, year in books:
+    db.execute("INSERT INTO BOOKS(isbn, title, author, year) VALUES(:isbn, :title, :author, :year)", {
+      "isbn": isbn,
+      "title": title,
+      "author": author,
+      "year": year
+    })
+    printf(f"Added {title} into books")
+db.commit()
